1754f7323c665060c357ef9ee9a814dea6a5389e
Yujin Lee <yjlee462611@gmail.com>
2025-06-04 01:47:01 +0900

    Feat: Layout 구성

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,17 @@
+export default function () {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        backgroundColor: "#F7F8FA",
+        height: "62px",
+        fontSize: "14px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      © Powered by yuyu
+    </div>
+  );
+}
